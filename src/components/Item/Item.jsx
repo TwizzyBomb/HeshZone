02bb6442de9d3ca57fdbac8af7b507b7b15bd8fb
Adrian <abrocke@hesh.zone>
2025-05-27 22:01:06 -0700

    Flex section fixed, Header re-adjusts image, footer stays put

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,7 +3,7 @@ import './Item.css'
 export default function Item({ itemId, title, description, url, image} ) {
   return (
     <div className="item-slot">
-      <a href={url} class="item-link" >
+      <a href={url} className="item-link" >
         <div className="item" id={itemId} >
           <h2>{title}</h2>
           <p>{description}</p>
