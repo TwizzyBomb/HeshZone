735661953adf785eb81d0d06178a4b7e3d3e5a27
Adrian <adrian.brocke@gmail.com>
2025-08-13 14:51:47 -0700

    quick import fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header/Header.jsx'
-import Item from './components/Item/Item.jsx'
 import AnimationComponent from './components/AnimationComponent.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import { DIGITAL_PORTFOLIO } from "./portfolio-data.js";
-import { ONLINE_PRESENCE } from "./presence-data.js";
 import './App.css'
 
 function App() {
